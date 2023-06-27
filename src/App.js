66b528f8083567cf90c3da5dc973c6954a6f095d
Kirill Pavliashik <kpavliashik@netevia.com>
2023-06-27 10:49:41 +0400

    added mui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import MagicForm from './components/MagicForm';
+import MagicFormMui from './components/MagicFormMui';
 import SimpleForm from './components/SimpleForm';
 
 function App() {
@@ -9,6 +10,8 @@ function App() {
       <header className="App-header">
         <SimpleForm />
         <MagicForm />
+        <MagicFormMui />
+        <div style={{padding: '20px 0'}}>P.S.: Magic = react-hook-form + yup</div>
       </header>
     </div>
   );
---
+++ src/components/MagicFormMui.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { Input } from '@mui/material';
+
+const style = {color: "red", paddingLeft: '10px'}
+
+const schema = yup.object().shape({
+  name: yup.string().required('Имя обязательно'),
+  email: yup.string().required('Email обязателен').email('Неверный формат email'),
+});
+
+const MagicFormMui = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data) => {
+    // Отправка данных формы
+    console.log('Форма отправлена');
+    // Сброс полей формы
+    reset();
+  };
+
+  return (
+    <div>
+      <h1>Magic MUI Форма:</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="name">Имя:</label>
+          <Input type="text" id="name" {...register('name')} />
+          {errors.name && <span style={style}>{errors.name.message}</span>}
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <Input type="text" id="email" {...register('email')} />
+          {errors.email && <span style={style}>{errors.email.message}</span>}
+        </div>
+        <button type="submit">Отправить</button>
+      </form>
+    </div>
+  );
+};
+
+export default MagicFormMui;
+
+
